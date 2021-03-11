7e487e1bd9275f816b633384bc05a1a211175cc8
AdrianGIII <78130592+AdrianGIII@users.noreply.github.com>
2021-03-10 22:48:37 -0800

    Update ascii2bin.c

--- ascii2bin.c
+++ ascii2bin.c
@@ -4,8 +4,8 @@
 /* Date: 2-12-21                */
 /********************************/
 
-#include "stdio.h"
-#include "stdlib.h"
+#include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <math.h> 
 
