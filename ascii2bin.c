d34e3c1825b368ee469eed3617eeb79b91255ed2
AdrianGIII <78130592+AdrianGIII@users.noreply.github.com>
2021-03-10 21:53:36 -0800

    Update ascii2bin.c

--- ascii2bin.c
+++ ascii2bin.c
@@ -6,6 +6,7 @@
 
 #include "stdio.h"
 #include "stdlib.h"
+#include <unistd.h>
 
 #define byte unsigned char
 
@@ -16,7 +17,7 @@ int main (int argc, char * argv[], char ** envp) {
     int digit = 0;
     int total = 0;
     int valueOfBit = 0;
-    int count = 1
+    int count = 1;
     int total2 = 0;
     
     int retval = read(0, &ascii_value, 1);
@@ -37,9 +38,9 @@ int main (int argc, char * argv[], char ** envp) {
 
 
         for (int i = 0; i < 8; i++) { // assummes 8 bits in one read. 
-            valueOfBit = (ascii_value / (int) pow(10, i)) - 10 * (ascii_value / (int)Math.pow(10, i + 1));
+            valueOfBit = (ascii_value / (int) pow(10, i)) - 10 * (ascii_value / (int) pow(10, i + 1));
             if (valueOfBit == 0 || valueOfBit == 1) {
-                total += ascii_value * Math.pow(2, i); // probaby makes dec value, look over this again
+                total += ascii_value * pow(2, i); // probaby makes dec value, look over this again
             }
             else {
                 fprintf(stderr, "Error Detected! NOT A BINARY NUM\n");
@@ -51,7 +52,7 @@ int main (int argc, char * argv[], char ** envp) {
         //(place before while) 
 
         if (ascii_value == 0 || ascii_value == 1){
-            total2 += ascii_value * Math.pow(2, count);
+            total2 += ascii_value * pow(2, count);
         } else {
             fprintf(stderr, "Error Detected! NOT A BINARY NUM (or not single bit intake)\n");
             //return 1;
