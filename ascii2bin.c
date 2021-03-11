da8aba2f9dc00e65a9ccba3af9693fd1f8efd83d
AdrianGIII <78130592+AdrianGIII@users.noreply.github.com>
2021-03-10 22:58:29 -0800

    Update ascii2bin.c

--- ascii2bin.c
+++ ascii2bin.c
@@ -16,21 +16,21 @@
 int main (int argc, char * argv[], char ** envp) {
     int offset = 0;
     int number = 0;
-    int ascii_value = 0;
+    int asciival = 0;
     int digit = 0;
     int total = 0;
     int valueOfBit = 0;
     int count = 1;
     int total2 = 0;
     
-    int retval = read(0, &ascii_value, 1);
-    fprintf(stdout, "ascii value = %d \n", ascii_value); 
+    int retval = read(0, &asciival, 1);
+    fprintf(stdout, "ascii value = %d \n", asciival); 
 	
     while (retval == 1){
 
 	    // This is where i do all my binary -> dec convertions
 
-        digit = ascii_value - offset;
+        digit = asciival - offset;
         fprintf(stdout, "Digit = %d\n", digit); 
         //tells me what the hell this thing does, and what ascii value is
 
@@ -42,10 +42,10 @@ int main (int argc, char * argv[], char ** envp) {
 
 
         for (int i = 0; i < 8; i++) { // assummes 8 bits in one read. 
-            valueOfBit = (ascii_value / (int) (pow(10, i)+.5)) - 10 * (ascii_value / (int) (pow(10, i + 1)+.5) );
+            valueOfBit = (asciival / (int) (pow(10, i)+.5)) - 10 * (asciival / (int) (pow(10, i + 1)+.5) );
 		fprintf(stdout, "valueOfBit = %d ", valueOfBit); 
             if (valueOfBit == 0 || valueOfBit == 1) {
-                total += ascii_value * (int) (pow(2, i)+.5); // probaby makes dec value, look over this again
+                total += asciival * (int) (pow(2, i)+.5); // probaby makes dec value, look over this again
             }
             else {
                 fprintf(stderr, "Error Detected! NOT A BINARY NUM\n");
@@ -69,7 +69,7 @@ int main (int argc, char * argv[], char ** envp) {
 
         
 
-        retval = read(0, &ascii_value, 1);
+        retval = read(0, &asciival, 1);
     }
     fprintf(stdout, "Total = %d \n", total);
     fprintf(stdout, "Total 2 = %d \n", total2);
