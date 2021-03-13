c8f4045cdc97fe326ba834c6998074687c1dfcf4
AdrianGIII <78130592+AdrianGIII@users.noreply.github.com>
2021-03-12 20:49:03 -0800

    Update ascii2bin.c

--- ascii2bin.c
+++ ascii2bin.c
@@ -29,7 +29,7 @@ int main (int argc, char *argv[], char ** envp)
     int total2 = 0;
 	
     while (retval == 1){
-	ascii_value -= offset
+	ascii_value -= offset;
 	//asciival = 10001010;
 
 	// This is where i do all my binary -> dec convertions
