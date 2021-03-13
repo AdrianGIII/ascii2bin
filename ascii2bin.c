c20d6abb4b2132f0af57177288acd83824f98fd5
AdrianGIII <78130592+AdrianGIII@users.noreply.github.com>
2021-03-12 21:32:37 -0800

    Update ascii2bin.c

--- ascii2bin.c
+++ ascii2bin.c
@@ -44,7 +44,7 @@ int main (int argc, char *argv[], char ** envp)
 	    expHelp = 2 * exp;
 	    exp = expHelp;
         } else {
-	    if (ascii_value == -1){
+	    if (ascii_value == -38){
 		    fprintf(stdout, " new line experiment succsess  ");
 	    }
             fprintf(stderr, "Error Detected! NOT A BINARY NUM \n");
